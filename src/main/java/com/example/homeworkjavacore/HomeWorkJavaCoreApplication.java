de49f5e22795034b52fc571950f770b1cf3762db
Khasan289 <sarmatovalan289@gmail.com>
2023-02-13 17:04:16 +0300

    Initial commit

---
+++ src/main/java/com/example/homeworkjavacore/HomeWorkJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkjavacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkJavaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkJavaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkjavacore/HomeWorkJavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkjavacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkJavaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
